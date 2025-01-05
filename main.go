0018400baaebc4db95874dc959ba3af3216b960a
Vini <contatoruanvinicius@gmail.com>
2025-01-05 19:25:16 -0300

    Finish

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"fmt"
-	rand "math/rand"
-
+	// rand "math/rand"
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 const (
-	width = 680
-	height = 680
+	width = 800
+	height = 800
 	cellSize = 40
 	rowsCount = height / cellSize
 	colsCount = width / cellSize
@@ -18,7 +16,8 @@ const (
 var (
 	gray = rl.NewColor(31, 35, 53, 255)
 	purple = rl.NewColor(157, 124, 216, 255)
-	cellsMatrix = []Cell{}
+	white = rl.NewColor(86, 95, 137, 255)
+	cellsMatrix = [rowsCount][colsCount]bool{}
 	cellSizeV2 = rl.NewVector2(cellSize, cellSize)
 	frameCounter = 0
 )
@@ -26,138 +25,156 @@ var (
 type Cell struct {
 	X float32;
 	Y float32;
-	Status bool;
+ bool;
 }
-
 
 func main() {
 	rl.InitWindow(width, height, "Game of Life")
 	defer rl.CloseWindow()
 
 	rl.SetTargetFPS(144)
-	cellsMatrix = initializeCells(cellsMatrix)
-
-	fmt.Printf("")
+	initializeCells(&cellsMatrix)
+	
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(gray)
-
 		
-		if frameCounter % 10 == 0 {
-			calculateStep()
-		}
-		for _, cell := range cellsMatrix {
-			cellV2 := rl.NewVector2(cell.X, cell.Y)
-			color := purple
-			if !cell.Status {
-				color = gray
+		for rowIndex, rowValue := range cellsMatrix {
+			for colIndex, cell := range rowValue {
+				cellV2 := rl.NewVector2(float32(colIndex)*cellSize, float32(rowIndex)*cellSize)
+				color := purple
+				if !cell {
+					color = gray
+				}
+				rl.DrawRectangleV(cellV2, cellSizeV2, color)
 			}
-			rl.DrawRectangleV(cellV2, cellSizeV2, color)
 		}
+
 		for i := 0; i <= rowsCount; i++ {
-			rl.DrawLine(int32(cellSize*i), 0, int32(cellSize*i), height, rl.White)
+			start := rl.NewVector2(float32(cellSize*i), 0)
+			end := rl.NewVector2(float32(cellSize*i), float32(height))
+			rl.DrawLineEx(start, end, float32(3), white)
 		}
 
 		for j := 0; j <= colsCount; j++ {
-			rl.DrawLine(0, int32(cellSize*j), width, int32(cellSize*j), rl.White)
+			start := rl.NewVector2(0, float32(cellSize*j))
+			end := rl.NewVector2(width, float32(cellSize*j))
+			rl.DrawLineEx(start, end, float32(3), white)
 		}
 
+		if frameCounter % 10 == 0 {
+			 calculateStep()
+		}
 		frameCounter++
 		rl.EndDrawing()
 	}
 }
 
-func initializeCells(cells []Cell) []Cell {
+func initializeCells(cells *[rowsCount][colsCount]bool) *[rowsCount][colsCount]bool {
 	for i := 0; i < rowsCount; i++ {
 		for j := 0; j < colsCount; j++ {
-			fmt.Println("Status: ", rand.Int() % 2 == 1)
-			cell := Cell { X: float32(i*cellSize), Y: float32(j*cellSize), Status: rand.Int() % 2 == 1}	
-			cells = append(cells, cell)
+			cells[i][j] = false
 		}
 	}
 
+	// cells[0][1] = true
+	// cells[1][2] = true
+	// cells[2][0] = true
+	// cells[2][1] = true
+	// cells[2][2] = true
+
+	cells[rowsCount/2][colsCount/2 + 2] = true
+	cells[rowsCount/2 + 1][colsCount/2 + 1] = true
+	cells[rowsCount/2 + 1][colsCount/2 + 2] = true
+	cells[rowsCount/2 + 2][colsCount/2 + 2] = true
+	cells[rowsCount/2 + 2][colsCount/2 + 3] = true
+		
 	return cells
 }
 
 func calculateStep() {
-	//Como que ele esquece do j + I * width
-	//linhas + colunas * tamanho da minha linha (quantidade de colunas)
-	//pode ser que seja colunas+linhas também, I e J são nomes idiotas.
-	for i := 0; i < rowsCount; i++ {
-		for j := 0; j < colsCount; j++ {
-			counter := calculateNeighbors(cellsMatrix, j, i, colsCount, rowsCount)	
-			cell := &cellsMatrix[j + i * colsCount]
+	newMatrix := cellsMatrix
 
-			fmt.Printf("X: %f, Y: %f, Neighbor: %d\n", cell.X, cell.Y, counter)
+	for rowIndex, rowValue := range cellsMatrix {
+		for colIndex, cellValue := range rowValue {
+			counter := calculateNeighbors(cellsMatrix, colIndex, rowIndex)	
+			// Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+			if counter < 2 &&  cellValue {
+				newMatrix[rowIndex][colIndex] = false
+				continue
+			}
 
-			if counter < 2 && cell.Status {
-				cell.Status = false
-			} else if cell.Status && (counter == 2 || counter == 3) {
-				cell.Status = true
-			} else if cell.Status && counter > 3 {
-				cell.Status = false
-			} else if !cell.Status && counter == 3 {
-				cell.Status = true
+			// Any live cell with two or three live neighbours lives on to the next generation
+			if cellValue && (counter == 2 || counter == 3) {
+				newMatrix[rowIndex][colIndex] = true
+				continue
+			}
+
+			// Any live cell with more than three live neighbours dies, as if by overpopulation
+			if cellValue && counter > 3 {
+				newMatrix[rowIndex][colIndex] = false
+				continue
+			}
+
+			// Any dead cell with exactly three live neighbours becomes a live cell as if by reproduction
+			if !cellValue && counter == 3 {
+				newMatrix[rowIndex][colIndex] = true
+				continue
 			}
 		}
 	}
+
+	cellsMatrix = newMatrix
 }
 
-func calculateNeighbors(cellsMatrix []Cell, column int, line int, colsCount int32, rowsCount int32) int {
+func calculateNeighbors(cellsMatrix [rowsCount][colsCount]bool, column int, line int) int {
 	counter := 0
-	
+
 	// full left neighbor
 	if 	column > 0 {
-		if cellsMatrix[column-1 + line * int(colsCount)].Status {
+		if cellsMatrix[line][column-1] {
 			counter+=1
 		}
 	}
-
 	// full right neighbor
-	if column < int(colsCount-1)  {
-		if cellsMatrix[(column+1) + line * int(colsCount)].Status {
+	if column < int(colsCount-1) {
+		if cellsMatrix[line][column+1] {
 			counter+=1
 		}
 	}
-
-	// top one
-	if line > 0 {
-		if cellsMatrix[column + (line-1) * int(colsCount)].Status {
-			counter+=1
-		}
-	}
-
-	// bottom one
-	if line < int(rowsCount-1) {
-		if cellsMatrix[column + (line+1) * int(colsCount)].Status {
-			counter+=1
-		}
-	}
-	
 	// top left
 	if line > 0 && column > 0 {
-		if cellsMatrix[(column-1) + (line-1) * int(colsCount)].Status {
+		if cellsMatrix[line-1][column-1] {
 			counter+=1
 		}
 	}
-
+	// top one
+	if line > 0 {
+		if cellsMatrix[line-1][column] {
+			counter+=1
+		}
+	}
 	// top right
 	if line > 0 && column < int(colsCount-1) {
-		if cellsMatrix[(column+1) + (line-1) * int(colsCount)].Status {
+		if cellsMatrix[line-1][column+1] {
 			counter+=1
 		}
 	}
-	
 	// bottom right
 	if line < int(rowsCount-1) && column < int(colsCount-1) {
-		if cellsMatrix[(column+1) + (line+1) * int(colsCount)].Status {
+		if cellsMatrix[line+1][column+1] {
 			counter+=1
 		}
 	}
-
+	// bottom one
+	if line < int(rowsCount-1) {
+		if cellsMatrix[line+1][column] {
+			counter+=1
+		}
+	}
 	// bottom left
 	if line < int(rowsCount-1) && column > 0 {
-		if cellsMatrix[(column-1) + (line+1) * int(colsCount)].Status {
+		if cellsMatrix[line+1][column-1] {
 			counter+=1
 		}
 	}
